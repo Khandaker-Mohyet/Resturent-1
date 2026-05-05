4ff796bd0d2c563e79cad0f6c5ae6584a4cb6532
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-05 21:27:32 +0600

    home page doen

---
+++ Js/script.js
@@ -0,0 +1,30 @@
+// document.getElementById('time').innerText = new Date()
+// setInterval(()=>{document.getElementById('time').innerText = new Date().toLocaleString(),1000})
+
+// console.log(1)
+// console.log(2)
+// setTimeout(()=>{
+//     console.log(2)
+// }, 5000)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+
+// let num = 0;
+// setInterval(()=>{
+//     document.getElementById('time').innerText = new Date().toLocaleString()
+// }, 1000)
+
+// document.getElementById('time').innerText = new Date().toLocaleString()
+
+// setInterval(()=>{
+//     document.getElementById('time').innerText = new Date().toLocaleString()
+// })
+
+
+
+setInterval(()=>{
+    document.getElementById('time').innerText = new Date().toLocaleString()
+})
+
