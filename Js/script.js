d43fae09f4cbebab294bc1ea5163d513f3384f30
Khandaker-Mohyet <mohyet008@gmail.com>
2026-05-05 23:07:12 +0600

    dish page

--- Js/script.js
+++ Js/script.js
@@ -1,30 +1,25 @@
-// document.getElementById('time').innerText = new Date()
-// setInterval(()=>{document.getElementById('time').innerText = new Date().toLocaleString(),1000})
-
-// console.log(1)
-// console.log(2)
-// setTimeout(()=>{
-//     console.log(2)
-// }, 5000)
-// console.log(3)
-// console.log(4)
-// console.log(5)
-
-
-// let num = 0;
-// setInterval(()=>{
-//     document.getElementById('time').innerText = new Date().toLocaleString()
-// }, 1000)
-
-// document.getElementById('time').innerText = new Date().toLocaleString()
-
-// setInterval(()=>{
-//     document.getElementById('time').innerText = new Date().toLocaleString()
-// })
-
-
 
 setInterval(()=>{
     document.getElementById('time').innerText = new Date().toLocaleString()
 })
+
+
+// const qty = document.getElementById("qty");
+
+// const increase = () => qty.value++;
+
+// const decrease = () => qty.value > 1 && qty.value--;
+
+
+  function increase() {
+    let qty = document.getElementById("qty");
+    qty.value++;
+  }
+
+  function decrease() {
+    let qty = document.getElementById("qty");
+    if (qty.value > 1) {
+      qty.value--;
+    }
+  }
 
